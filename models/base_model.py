494de6eb92530bd7c1ac1407ae08eb355bd0f3b5
PhetlaDaCoder <hsphetla@gmail.com>
2024-11-12 04:26:03 -0800

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines the BaseModel for all common attributes/methods for other classes."""
+
+    def __int__(self, *args, **kwargs):
+        """Starts a new BaseModel.
+
+        Args:
+            *args: Null or unused.
+            **kwargs: key & value of the attributes.
+        """
+        form = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v  in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v,form)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+
+        data = self.__dict__.copy()
+        data["created_at"] = self.created_at.isoformat()
+        data["updated_at"] = self.updated_at.isoformat()
+        data["__class__"] = self.__class__.__name__
+        return data
+
+    def __str__(self):
+        """Should print String of the BaseModel."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
